e1705a41fad28fc730a335603298b67a9ad16bdc
Maicon Sanson Penna <maiconsanson@gmail.com>
2023-11-01 08:45:54 -0300

    refac/main: remove 'use client' directive and unused 'useRouter' from Link component

--- src/app/components/Link/index.js
+++ src/app/components/Link/index.js
@@ -1,10 +1,6 @@
-"use client";
-
-import { Link as LinkI18n, useRouter } from "@/i18n/navigation";
+import { Link as LinkI18n } from "@/i18n/navigation";
 
 export default function Link({ children, href, locale }) {
-  const router = useRouter();
-
   return (
     <LinkI18n href={href} locale={locale} prefetch={false}>
       {children}
