a02e4ca1396c56cb80fff7433bd663aac750d0e0
Maicon Sanson Penna <maiconsanson@gmail.com>
2023-11-01 09:11:17 -0300

    refac/main: make the 'locale' prop conditional

--- src/app/components/Link/index.js
+++ src/app/components/Link/index.js
@@ -2,7 +2,7 @@ import { Link as LinkI18n } from "@/i18n/navigation";
 
 export default function Link({ children, href, locale }) {
   return (
-    <LinkI18n href={href} locale={locale} prefetch={false}>
+    <LinkI18n href={href} {...(locale && { locale })} prefetch={false}>
       {children}
     </LinkI18n>
   );
